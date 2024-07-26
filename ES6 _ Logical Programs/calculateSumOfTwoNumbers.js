e6f3c94bcc1b3860d04e3e7d057ec736d56303dc
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-07-26 16:45:19 +0530

    Write a javascript function to calculate the sum of two numbers
    Write a javascript function to calculate the sum of two numbers

---
+++ ES6 _ Logical Programs/calculateSumOfTwoNumbers.js
@@ -0,0 +1,9 @@
+// 1.Write a javascript function to calculate the sum of two numbers
+
+function calculateSum(a,b){
+    return a+b;
+}
+var num1 = 5;
+var num2 = 10;
+var sum = calculateSum(num1, num2);
+console.log('The sum is', +sum);
