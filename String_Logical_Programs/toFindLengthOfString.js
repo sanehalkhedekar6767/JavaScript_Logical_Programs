2a084742ebf2683105e4f1c8c68fc3e25af40198
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-07-26 16:44:04 +0530

    WAP to find the length or total number of characters of the strings
    WAP to find the length or total number of characters of the strings

---
+++ String_Logical_Programs/toFindLengthOfString.js
@@ -0,0 +1,12 @@
+// 1. WAP to find the length or total number of characters of the strings
+// ○ const str = "Hello, World!";
+// ○ const str = "JavaScript is the language of Internet”
+
+ const str1 = "Hello, World!";
+ const length1 = str1.length;
+ console.log(`The length of the string '${str1}' is: ${length1}`);
+
+ const str2 = "JavaScript is the language of Internet";
+ const length2 = str2.length;
+ console.log(`The length of the string '${str2}' is: ${length2}`);
+ 
