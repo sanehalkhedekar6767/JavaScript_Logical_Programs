d218a1ce4662070a9b53b8f77228c4c75ebc57da
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-07-26 15:12:11 +0530

    1. Javascript program to print the number of an element present in array
     1. Javascript program to print the number of an element present in array

---
+++ Array_Logical_Programs/noOfElementInArray.js
@@ -0,0 +1,7 @@
+// Javascript program to print the number of an element present in array
+
+function printNumberOfElements(arr){
+    console.log('Number of elements in array:', arr.length);
+}
+let array = [1,2,3,4,5,6];
+printNumberOfElements(array);
